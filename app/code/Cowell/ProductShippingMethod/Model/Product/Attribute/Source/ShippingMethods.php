fa718f8fd70db4c2c7f0f72954b1efe5672497b5
victor <vinhnx7391@co-well.vn>
2022-12-16 02:32:32 +0700

    Create Attribute Product: ShippingMethods

---
+++ app/code/Cowell/ProductShippingMethod/Model/Product/Attribute/Source/ShippingMethods.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Cowell\ProductShippingMethod\Model\Product\Attribute\Source;
+
+class ShippingMethods extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
+{
+    protected $scopeConfig;
+    protected $shipconfig;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Shipping\Model\Config $shipconfig
+    ) {
+        $this->shipconfig = $shipconfig;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function getAllOptions()
+    {
+        $activeCarriers = $this->shipconfig->getActiveCarriers();
+
+        if (!$this->_options) {
+            foreach ($activeCarriers as $carrierCode => $carrierModel) {
+                $options = array();
+                if ($carrierMethods = $carrierModel->getAllowedMethods()) {
+                    foreach ($carrierMethods as $methodCode => $method) {
+                        $code = $methodCode;
+                        $options[] = array('value' => $code, 'label' => $method);
+                    }
+                    $carrierTitle = $this->scopeConfig
+                        ->getValue('carriers/'.$carrierCode.'/title');
+                    $this->_options[] = array('value' => $options, 'label' => $carrierTitle);
+                }
+            }
+        }
+        return $this->_options;
+    }
+}
